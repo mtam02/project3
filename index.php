51425b332db4f3d55b9a068d3a01a8d50e192d50
mtam02 <mtam2@u.rochester.edu>
2016-12-25 01:08:02 -0500

    Home - Sections 3 & 4; Content & CSS Altered

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 
 				        	<div class="accomplishments-text">
 				        		<h3>Eastman School of Music</h3>
-				        		<p>Bachelor of Music in Applied Music (Performance) – Violin, May 2017</p>
+				        		<p>Bachelor of Music in Applied Music (Performance) – Violin | May 2017</p>
 				        	</div>
 
 				        </div>
@@ -60,7 +60,7 @@
 
 				        	<div class="accomplishments-text">
 				        		<h3>Hong Kong Polytechnic University</h3>
-				        		<p>Higher Diploma in Multimedia Design and Technology (Attended first academic year only), June 2012</p>
+				        		<p>Higher Diploma in Multimedia Design and Technology (Attended first academic year only) | June 2012</p>
 				        	</div>
 
 				        </div>
@@ -73,7 +73,7 @@
 
 				        	<div class="accomplishments-text">
 						        <h3>Diocesan Boys’ School</h3>
-						        <p>High School Diploma, March 2011</p>
+						        <p>High School Diploma | March 2011</p>
 						    </div>
 
 						</div>
@@ -86,8 +86,8 @@
 
 				        	<div class="accomplishments-text">
 						        <h3>Trinity College London</h3>
-						        <p>Licentiate Diploma in Recital in Violin with Distinction, December 2005</p>
-						        <p>Associate Diploma in Recital in Violin with Distinction, November 2003</p>
+						        <p>Licentiate Diploma in Recital in Violin with Distinction | December 2005</p>
+						        <p>Associate Diploma in Recital in Violin with Distinction | November 2003</p>
 						    </div>
 
 						</div>
@@ -101,8 +101,8 @@
 
 				        	<div class="accomplishments-text">
 						        <h3>The Associated Board of the Royal Schools of Music</h3>
-						        <p>Grade 8 Violin with Distinction, December 2002</p>
-						        <p>Grade 5 Theory of Music, February 2002</p>
+						        <p>Grade 8 Violin with Distinction | December 2002</p>
+						        <p>Grade 5 Theory of Music | February 2002</p>
 					        </div>
 
 					    </div>
@@ -116,6 +116,118 @@
         </section>
 
         <!-- Third Section -->
+
+        <section id="performances">
+
+			<div class="container">
+
+        		<div class="overlay-content-container white-overlay">
+
+        			<h2>Performances</h2>
+
+        			<div class="overlay-content">
+
+			        	<div class="accomplishments">
+
+				        	<div class="accomplishments-img">
+				        		<img src="images/esm.png" alt="esm">
+				        	</div>
+
+				        	<div class="accomplishments-text">
+				        		<h3>Eastman Philharmonia</h3>
+				        		<p>Ravel: Rapsodie espagnole; Kevin Puts: “Letters from Georgia”; Prokofiev: Symphony No. 5 in B-flat Major, Op. 100 | Alice Tully Hall at Lincoln Center, November 14, 2016</p>
+				        		<p>World premiere of Kevin Puts’ “Letters from Georgia” with Ms. Renée Fleming | Kodak Hall at Eastman Theatre | November 12, 2016</p>
+				        	</div>
+
+				        </div>
+
+				        <div class="accomplishments">
+
+				        	<div class="accomplishments-img">
+				        		<img src="images/bowdoin.png" alt="bowdoin">
+				        	</div>
+
+				        	<div class="accomplishments-text">
+				        		<h3>Bowdoin International Music Festival</h3>
+				        		<p>Bartók: Violin Concerto No. 2, Sz. 112 - I. Allegro non troppo | Studzinski Recital Hall | August 5, 2016</p>
+				        		<p>Brahms: Clarinet Quintet in B minor, Op. 115 - I. Allegro | Studzinski Recital Hall | August 4, 2016</p>
+				        	</div>
+
+				        </div>
+
+				        <div class="accomplishments">
+
+				        	<div class="accomplishments-img">
+				        		<img src="images/esm.png" alt="esm">
+				        	</div>
+
+				        	<div class="accomplishments-text">
+						        <h3>Violin Master Class with Mr. Sungsic Yang</h3>
+						        <p>Bruch: Scottish Fantasy, Op. 46 - II. Allegro; IV. Finale: Allegro guerriero | Howard Hanson Hall | October 12, 2015</p>
+						    </div>
+
+						</div>
+
+				        <div class="accomplishments">
+
+				        	<div class="accomplishments-img">
+				        		<img src="images/bowdoin.png" alt="bowdoin">
+				        	</div>
+
+				        	<div class="accomplishments-text">
+						        <h3>Bowdoin International Music Festival</h3>
+						        <p>Prokofiev: Violin Concerto No. 1 in D, Op. 19 - I. Andantino; II. Scherzo (Vivacissimo) | Studzinski Recital Hall | August 6, 2015</p>
+						        <p>Tchaikovsky String Sextet in D minor “Souvenir de Florence”, Op. 70 - I. Allegro con spirito; IV. Allegro vivace | Studzinski Recital Hall | August 2&7, 2015</p>
+					        </div>
+
+					    </div>
+
+        			</div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <!-- Fourth Section -->
+
+        <section id="computing">
+
+			<div class="container">
+
+        		<div class="overlay-content-container white-overlay">
+
+        			<h2>Computing</h2>
+
+        			<div class="overlay-content">
+
+			        	<div class="accomplishments">
+
+				        	<div class="accomplishments-img">
+				        		<img src="images/ur.png" alt="ur">
+				        	</div>
+
+				        	<div class="accomplishments-text">
+				        		<h3>Advanced Front-end Web Design and Development</h3>
+				        		<p>HTML, CSS and JavaScript: Information architecture, visual design, use of client libraries (mostly JavaScript), and asset management strategies; PHP and MySQL: Content Management Systems and web databases | Fall 2016</p>
+				        		<h3>Web Design and Development</h3>
+				        		<p>Internet and Web technologies: Internet transport protocols, HTML5 and CSS3, web page design and website publishing, with fundamentals, design concepts and industry standards; User experience, mobile design issues, and copyright/intellectual property considerations | Spring 2016</p>
+				        		<h3>The Art of Programming</h3>
+				        		<p>Python programming language: Core concepts and techniques of programming | Fall 2015</p>
+				        		<h3>Computer Applications</h3>
+				        		<p>Computer graphics, file compression and animation; Digital data in spreadsheets, presentations and data processing tools; Two-and-three dimensional (images and geometry) data structures with tools for data visualization; Video and audio editing tools</p>
+				        	</div>
+
+				        </div>
+
+        			</div>
+
+                </div>
+
+            </div>
+
+        </section>
 
 		<?php include "inc/footer.inc";?>
 
